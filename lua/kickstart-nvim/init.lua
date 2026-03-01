cdffb649b64b59c34ed4c26bc6ac125a1871d990
A-train134 <janthonyhitch@gmail.com>
2026-02-28 23:38:37 -0500

    help

--- lua/kickstart-nvim/init.lua
+++ lua/kickstart-nvim/init.lua
@@ -196,6 +196,7 @@ vim.keymap.set("n", "<leader>tt", ":tabnew .<CR>", { desc = "[t]abnew" })
 vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { desc = "[t]ab [c]lose" })
 vim.keymap.set("i", "<C-M>", "<Down>", { desc = "Move focus to the upper window" })
 vim.keymap.set("n", "<C-B>", ":vert term<CR>", { desc = "Open a new Vertical Window Terminal" })
+vim.keymap.set("i", "<C-BS>", "db", { desc = "Ctrl backspace" })
 -- vim.keymap.set("n", "shift")
 --  See `:help lua-guide-autocommands`
 
